5060c043e1f4c3c44f49076f3b1ea3b403bd4ae8
Ali Almohaya <alialmohaya@gmail.com>
2018-03-12 15:45:07 +0300

    suuport the files loader

--- webpack.config-helper.js
+++ webpack.config-helper.js
@@ -46,9 +46,18 @@ module.exports = (options) => {
             presets: ['es2015']
           }
         }
+      }, {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
       }]
     }
-    };
+  };
 
   if (options.isProduction) {
     webpackConfig.entry = ['./src/scripts/index'];
