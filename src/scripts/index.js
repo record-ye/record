42fd68f74018c99173a92d88519046a8f006eb4e
Ali Almohaya <alialmohaya@gmail.com>
2018-03-18 19:13:34 +0300

    add some scripting

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,3 +8,40 @@ import 'babel-polyfill';
 
 // styles
 import '../styles/index.scss';
+
+
+$(function () {
+  // jQuery is ready
+
+  // when the images are loaded
+  $(window).on('load', function () {
+    $('#loader').remove();
+  });
+
+
+  $(document).on('scroll', function () {
+    console.log(window.pageYOffset);
+    if (window.pageYOffset >= ($('#main-header').offset().top + $('#main-header').height() + 35)) {
+      $('#main-nav-bar').addClass('active');
+    } else {
+      $('#main-nav-bar').removeClass('active');
+    }
+  });
+
+
+  // grid view for the port
+  const $grid = $('#portfolio-grid').isotope({
+    // options
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows'
+  });
+
+  $('#selector li').on('click', function () {
+    $grid.isotope({
+      filter: '.' + $(this).text().toLowerCase()
+    });
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+
+
+});
